f53ad3cf9ed9cbdfa3e0533c44e96a416dec3eb6
ehtishamfaisal <ehtishamfaisal@gmail.com>
2017-03-22 23:17:31 +0500

    object manipulated

--- main.js
+++ main.js
@@ -7,22 +7,15 @@ $(document).ready(function() {
 		$('#sampleArea').html(info);
 	});
 
-	// var Prod = {
-	// 	name: 'ani',
-	// 	product: 'ball',
-	// 	qty: 15
-	// }
-	// Prod.onsale = function() {
-	// 	this.qty-1;
-	// 	console.log(this.qty);
-	// };
-
-	// Prod.onpurchase = function() {
-	// 	this.qty+1;
-	// 	console.log(this.qty);
-	// };
-
-	// var template = $('#employeeTpl').html();
-	// var info = Mustache.to_html(template, Prod);
-	// $('#sampleArea').html(info);	
+var DATA = {
+	name: "Christophe",
+	price: 100,
+	salesTax: 0.05,
+	add:function() {
+		return this.price + this.price * this.salesTax;
+	}
+    }
+	var template = $('#productTpl').html();
+	var info = Mustache.to_html(template, DATA);
+	$('#Area').html(info);
 });
